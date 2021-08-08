1e25ab8a03939161a1c465f49c6803e570be39bb
imildositoe <imildositoe@gmail.com>
2021-08-08 02:28:09 +0200

    dependencies problems with template solved

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  modules: {
+    rules: [
+      {test: /\.(jpe?g|png|gif|svg)$/i, loader: "file-loader?name=app/images/[name].[ext]"},
+    ]
+  },
   presets: [
     '@vue/cli-plugin-babel/preset'
   ]
--- src/main.js
+++ src/main.js
@@ -1,5 +1,40 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import "src/components/assets/images/favicon.png"
+
+// CSS
+import "./components/assets/vendor/bootstrap/dist/css/bootstrap.min.css"
+import "./components/assets/vendor/swiper/swiper-bundle.min.css"
+import "./components/assets/vendor/fancybox/dist/jquery.fancybox.min.css"
+import "./components/assets/vendor/slider-revolution/css/settings.css"
+import "./components/assets/vendor/slider-revolution/css/layers.css"
+import "./components/assets/vendor/slider-revolution/css/navigation.css"
+import "./components/assets/css/monsterplay.min.css"
+import "./components/assets/css/custom.css"
+
+
+// JS
+import "./components/assets/vendor/jquery/dist/jquery.min"
+import "./components/assets/js/preloader.min"
+import "./components/assets/vendor/object-fit-images/dist/ofi.min"
+import "./components/assets/vendor/popper.js/dist/umd/popper.min"
+import "./components/assets/vendor/scrollreveal/dist/scrollreveal.min"
+import "./components/assets/vendor/animejs/lib/anime.min"
+import "./components/assets/vendor/bootstrap/dist/js/bootstrap.min"
+import "./components/assets/vendor/jarallax/dist/jarallax.min"
+import "./components/assets/vendor/swiper/swiper-bundle.min"
+import "./components/assets/vendor/fancybox/dist/jquery.fancybox.min"
+import "./components/assets/vendor/jquery-countdown/dist/jquery.countdown.min"
+import "./components/assets/vendor/moment/min/moment.min"
+import "./components/assets/vendor/moment-timezone/builds/moment-timezone-with-data.min"
+import "./components/assets/vendor/slider-revolution/js/jquery.themepunch.tools.min"
+import "./components/assets/vendor/slider-revolution/js/jquery.themepunch.revolution.min"
+import "./components/assets/vendor/imagesloaded/imagesloaded.pkgd.min"
+import "./components/assets/vendor/isotope-layout/dist/isotope.pkgd.min"
+import "./components/assets/vendor/ion-rangeslider/js/ion.rangeSlider.min"
+import "./components/assets/vendor/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min"
+import "./components/assets/vendor/bootstrap-validator/dist/validator.min"
+import "./components/assets/js/monsterplay.min"
+import "./components/assets/js/monsterplay-init"
+
 
 createApp(App).mount('#app')
