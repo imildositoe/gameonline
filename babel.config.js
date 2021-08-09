4877b8e2a7f1d2433764e60e85c53845cf017a0f
imildositoe <imildositoe@gmail.com>
2021-08-09 12:09:59 +0200

    TEMPLATE LOADED PROPERLY INTO PROJECT

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  modules: {
-    rules: [
-      {test: /\.(jpe?g|png|gif|svg)$/i, loader: "file-loader?name=app/images/[name].[ext]"},
-    ]
-  },
   presets: [
     '@vue/cli-plugin-babel/preset'
   ]
